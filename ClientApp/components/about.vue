6fd02d273c739fdd42196b32d3566cea5f655760
Richard Johnn <rick.johnn@gmail.com>
2019-04-09 08:50:47 -0400

    Minor cleanup

--- ClientApp/components/about.vue
+++ ClientApp/components/about.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
-      <h1 class="mt-4">
-        ASP.NET Core & Vue Starter Template
-      </h1>
-    </div>
+  <div>
+    <h1 class="mt-4">
+      ASP.NET Core - Vue Starter Template
+    </h1>
+    <hr/>
+    All about foo
+  </div>
 </template>
 
 <script>
--- ClientApp/components/home-page.vue
+++ ClientApp/components/home-page.vue
@@ -3,15 +3,6 @@
       <h1 class="mt-4">
         ASP.NET Core - Vue Starter Template
       </h1>
-
-      <h5 class="mt-5">
-        Made with <icon icon="heart" class="fuscia" /> by
-        "Foobar"
-      </h5>
-
-      <br>
-
-     
     </div>
 </template>
 
